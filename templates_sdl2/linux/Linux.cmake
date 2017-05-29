952317eccfa6254e5180d0a6565b9a7756d53d87
Parley Martins <parleypachecomartins@gmail.com>
2017-05-29 15:15:31 -0300

    Start sdl2 script

---
+++ templates_sdl2/linux/Linux.cmake
@@ -0,0 +1,13 @@
+cmake_minimum_required(VERSION 2.6)
+
+set(SYSTEM linux)
+
+# External libraries configure options
+set(SDL_CONFIGURE_OPTIONS --enable-video-x11 --enable-x11-shared --enable-video-x11-vm)
+# set(SDL_IMAGE_CONFIGURE_OPTIONS "")
+# set(SDL_NET_CONFIGURE_OPTIONS "")
+# set(SDL_MIXER_CONFIGURE_OPTIONS "")
+# set(FREETYPE_CONFIGURE_OPTIONS "")
+# set(SDL_TTF_CONFIGURE_OPTIONS "")
+
+set(SYSTEM_LIBRARIES libSDL2.so libSDL2_image.so libSDL2_ttf.so libSDL2_mixer.so libSDL2_gfx.so)
---
+++ templates_sdl2/linux/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Generate the game executable for Linux platform.
+#
+# Author: Edson Alves - edsonalves@unb.br
+# Date: 09/03/2017
+
+# System
+SYSTEM=linux
+
+# Directories
+BIN_DIR=bin
+SRC_DIR=$src_folder
+LIB_DIR=lib
+BUILD_DIR=build
+
+SYSTEM_BIN_DIR=$BIN_DIR/$SYSTEM
+SYSTEM_LIB_DIR=$LIB_DIR/$SYSTEM
+SYSTEM_BIN_LIB_DIR=$SYSTEM_BIN_DIR/lib
+SYSTEM_BUILD_DIR=$BUILD_DIR/$SYSTEM
+
+SDL_DIR=$SYSTEM_LIB_DIR/SDL2
+SDL_LIB_DIR=$SDL_DIR/lib
+
+# FREETYPE_DIR=$SYSTEM_LIB_DIR/FreeType-2.4
+# FREETYPE_LIB_DIR=$FREETYPE_DIR/lib
+
+mkdir -p $BUILD_DIR/$SYSTEM $BIN_DIR/$SYSTEM/lib
+cd $BUILD_DIR/$SYSTEM
+pwd
+cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../../linux/Linux.cmake
+make
+cd ../..
+cp $SYSTEM_BUILD_DIR/$SRC_DIR/$project_name $SYSTEM_BIN_DIR
+cp $SDL_LIB_DIR/libSDL2-2.0.so $SYSTEM_BIN_LIB_DIR
+cp $SDL_LIB_DIR/libSDL2_image-2.0.so.0 $SYSTEM_BIN_LIB_DIR
+cp $SDL_LIB_DIR/libSDL2_ttf-2.0.so.0 $SYSTEM_BIN_LIB_DIR
+cp $SDL_LIB_DIR/libSDL2_mixer-2.0.so.0 $SYSTEM_BIN_LIB_DIR
+cp $SDL_LIB_DIR/libSDL2_gfx-1.0.so.0 $SYSTEM_BIN_LIB_DIR
+# cp $FREETYPE_LIB_DIR/libfreetype.so.6.5.0 $SYSTEM_BIN_LIB_DIR
